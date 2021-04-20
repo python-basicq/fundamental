fa39c4e1370c8761274353a46a24d70ad6cb82c3
redhat <redhanoormansyah30@gmail.com>
2021-04-20 15:32:40 +0700

    initial Commit

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+# Kamus Bahasa Inggris
+kamus_ind_eng = {'anak':'son', 'istri':'wife', 'ayah':'father'}
+
+print(kamus_ind_eng['anak'])
+data_server_gojek = {
+    'tanggal': '10-10-2021',
+    'driver_data': [
+        {'nama':'wargo','jarak':100},
+        {'nama':'jaruhi','jarak':350},
+        {'nama':'wisnuya','jarak':300},
+        {'nama':'dudung','jarak':300},
+        {'nama':'dudung','jarak':300}
+    ]
+}
+
+print(f" Data Ojol di Sekitarmu {data_server_gojek['driver_data'][0]}")
+print(f" Data Ojol di Sekitarmu {data_server_gojek['driver_data'][3]}")
+print(f" Data Ojol di Sekitarmu {data_server_gojek['driver_data'][1]}")
